51b4c9958fc7147dfad69174330c72dd04a8767a
lcr-yeyan <56009337+lcr-yeyan@users.noreply.github.com>
2024-11-05 11:08:05 +0800

    update

--- run.py
+++ run.py
@@ -1,20 +1,46 @@
 from GUI import GUI
+import cv2
+
 
 user_input_values = {}  # init dict
 
 
 def confirm(denoise, superres, filter_method, model, output_path, video_path):
     # store value into dict
-    user_input_values['denoise'] = denoise
-    user_input_values['superres'] = superres
-    user_input_values['filter_method'] = filter_method
-    user_input_values['model'] = model
-    user_input_values['output_path'] = output_path
-    user_input_values['video_path'] = video_path
+    user_input_values['denoise'] = denoise  # bool
+    user_input_values['superres'] = superres  # bool
+    user_input_values['filter_method'] = filter_method  # string
+    user_input_values['model'] = model  # string
+    user_input_values['output_path'] = output_path  # string
+    user_input_values['video_path'] = video_path  # string
 
-    # print
+    # print parameters
     print("Stored Values:")
     print(user_input_values)
+
+    # 错误判定
+    if output_path or video_path == '':
+        print("Warning: File path isn't defined")
+
+    # read video file
+    cap = cv2.VideoCapture(user_input_values['video_path'])
+    if not cap.isOpened():
+        print("Error: Could not find the video file")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        # use filter
+
+    cap.release()
+
+    # use super-resolution
+
+    # use model
 
 
 GUI.run_app(confirm)
